534f76d8e01d7fa008e4a18b90dfae3f82108304
DeeDloh <DeeD-2.0@ya.ru>
2021-11-18 18:56:03 +0300

    add not working version

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
 import sys
 
 from PyQt5 import uic
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QApplication, QWidget
+from random import randint
 
 class Yellow_circle_wid(QWidget):
     def __init__(self):
         super().__init__()
-        uic.loadUi("main.ui", self)
+        uic.loadUi("UI.ui", self)
+
+    def initUI(self):
+        self.setWindowTitle('Рисование')
+        self.do_paint = False
+        self.drw.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.update()
+
+    def draw_flag(self, qp):
+        k = randint(30, 50)
+        qp.setBrush(QColor(255, 255, 0))
+        qp.drawEllipse(200, 280, k, k)
+        self.do_paint = False
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
